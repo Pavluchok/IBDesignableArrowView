d9049524275bbe0f5ec619785775c619385a01cd
Vitaliy.Pavlyuk <pavluchokv@gmail.com>
2015-01-20 20:44:38 +0200

    Initial Commit

---
+++ IBDesignebleArrowView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignebleArrowView
+//
+//  Created by Vitaliy on 20.01.15.
+//  Copyright (c) 2015 Vitaliy Pavlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
